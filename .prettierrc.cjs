513dd6b40567bb6ff4b42d87a6b9e6fbf6b06552
Libor Gabrhel <gabrhel.libor@gmail.com>
2023-11-11 17:49:45 +0100

    chore: initialised app

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+/** Prettier configuration https://prettier.io/docs/en/configuration.html */
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 120,
+  useTabs: false,
+}
---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+/** Autoprefixer configuration https://github.com/postcss/autoprefixer#options */
+
+/** @type {import("postcss").Config} */
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+  },
+};
