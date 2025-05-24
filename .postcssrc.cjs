6f21a966dd36de054890c4ae8e1768ed692e3beb
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-05-24 14:28:25 +0200

    chore: added postcss and autoprefixer

---
+++ .postcssrc.cjs
@@ -0,0 +1,8 @@
+/** PostCSS configuration https://github.com/postcss/postcss#usage */
+
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+  },
+}
