519111e79d310b2e0b05b2315765c440e5a0ce1a
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-11-23 12:17:10 +0100

    chore: add Storybook dependencies and configuration
    - Install Storybook 10.0.8 with React-Vite framework
    - Add accessibility addon (@storybook/addon-a11y)
    - Add Vitest addon for component testing
    - Add autodocs and onboarding addons
    - Use exact versions for all Storybook packages
    - Update .gitignore for Storybook build artifacts
    - Update ESLint config for Storybook plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ module.exports = {
   ignorePatterns: ["!**/.server", "!**/.client"],
 
   // Base config
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:storybook/recommended"],
 
   overrides: [
     // React
