babf37cbcbf398d836e0d517d0a5df956f57c5c5
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-11-23 19:56:36 +0100

    refactor: simplify website layout and app-header
    - Made AppHeader children and isInEditMode props optional with defaults
    
    - Simplified website layout to remove authentication logic
    
    - Removed loader from website layout (auth moved to admin layout)
    
    - Website layout now only renders basic header, body, and footer

--- app/routes/website/__layout/route.tsx
+++ app/routes/website/__layout/route.tsx
@@ -1,43 +1,20 @@
 // noinspection JSUnusedGlobalSymbols
 
 import { Outlet } from "react-router"
-import { HoneypotProvider } from "remix-utils/honeypot/react"
 
-import { AdministrationPanel } from "~/components/administration-panel"
 import { AppBody } from "~/components/app-body"
 import { AppFooter } from "~/components/app-footer"
 import { AppHeader } from "~/components/app-header"
-import { AuthenticityTokenProvider } from "~/components/authenticity-token-provider"
 import "./_styles.css"
 
-import type { Route } from "./+types/route"
-
-export { loader } from "./_loader"
-
-export default function RouteComponent({ loaderData }: Route.ComponentProps) {
-  const isAuthenticated = loaderData?.isAuthenticated ?? false
-  const user = {
-    name: loaderData?.user.name,
-    email: loaderData?.user.email,
-    image: {
-      id: loaderData?.user.image.id,
-      altText: loaderData?.user.image.altText,
-    },
-  }
-
+export default function RouteComponent() {
   return (
     <>
-      <HoneypotProvider {...loaderData.honeypotInputProps}>
-        <AuthenticityTokenProvider token={loaderData.csrfToken}>
-          <AppHeader isInEditMode={isAuthenticated}>
-            {isAuthenticated ? <AdministrationPanel user={user} /> : null}
-          </AppHeader>
-          <AppBody>
-            <Outlet />
-          </AppBody>
-          <AppFooter />
-        </AuthenticityTokenProvider>
-      </HoneypotProvider>
+      <AppHeader />
+      <AppBody>
+        <Outlet />
+      </AppBody>
+      <AppFooter />
     </>
   )
 }
