6cc5f4b1bbfbd942519c6ea7b8483a26ccfd2ef2
Libor Gabrhel <gabrhel.libor@gmail.com>
2025-12-23 21:32:41 +0100

    feat: create admin entries intersection utility for route configuration

---
+++ utils/create-admin-entries-intersection.ts
@@ -0,0 +1,41 @@
+import {
+  index,
+  layout,
+  prefix,
+  type RouteConfigEntry,
+  route,
+} from '@react-router/dev/routes'
+
+type Args = {
+  name: string
+  entry: string
+  path: string
+  id: string
+}
+
+export const createAdminEntriesIntersection = (
+  { name, entry, path, id }: Args,
+  ...additionalRouteConfigEntries: RouteConfigEntry[]
+) => [
+  ...prefix(name, [
+    // Section layout
+    layout(`${path}/__layout/route.tsx`, [
+      // List
+      index(`${path}/_index/route.tsx`),
+      // Add entry
+      route(`add-${entry}`, `${path}/add-${entry}/route.tsx`),
+      // Entry
+      ...prefix(`:${id}`, [
+        // Entry layout
+        layout(`${path}/${entry}/__layout/route.tsx`, [
+          // Details
+          index(`${path}/${entry}/_index/route.tsx`),
+          // Edit entry
+          route(`edit-${entry}`, `${path}/${entry}/edit-${entry}/route.tsx`),
+        ]),
+      ]),
+
+      ...additionalRouteConfigEntries,
+    ]),
+  ]),
+]
