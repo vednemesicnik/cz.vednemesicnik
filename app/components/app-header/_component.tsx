192574674549310dd374186032b108af44b557a9
Libor Gabrhel <gabrhel.libor@gmail.com>
2026-02-19 15:16:52 +0100

    feat: update navigation links to use href for improved routing consistency

--- app/components/app-header/_component.tsx
+++ app/components/app-header/_component.tsx
@@ -1,9 +1,8 @@
 import type { ReactNode } from 'react'
-
+import { href } from 'react-router'
 import { HomeLink } from '~/components/app-header/components/home-link'
 import { Navigation } from '~/components/app-header/components/navigation'
 import { NavigationItem } from '~/components/app-header/components/navigation-item'
-
 import styles from './_styles.module.css'
 
 type Props = {
@@ -18,10 +17,10 @@ export const AppHeader = ({ children, isInEditMode = false }: Props) => {
       <section className={styles.content}>
         <HomeLink isInEditMode={isInEditMode} />
         <Navigation>
-          <NavigationItem to={'/articles'}>Články</NavigationItem>
-          <NavigationItem to={'/podcasts'}>Podcasty</NavigationItem>
-          <NavigationItem to={'/archive'}>Archiv</NavigationItem>
-          <NavigationItem to={'/editorial-board'}>Redakce</NavigationItem>
+          <NavigationItem to={href('/articles')}>Články</NavigationItem>
+          <NavigationItem to={href('/podcasts')}>Podcasty</NavigationItem>
+          <NavigationItem to={href('/archive')}>Archiv</NavigationItem>
+          <NavigationItem to={href('/editorial-board')}>Redakce</NavigationItem>
         </Navigation>
       </section>
     </header>
