0dc86bdf5aa1d5bdf223754e5dc3a3da2637393d
Libor Gabrhel <gabrhel.libor@gmail.com>
2026-04-09 16:34:35 +0200

    chore: remove Storybook addon and update Vite configuration for improved path resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,6 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   addons: [
-    '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
